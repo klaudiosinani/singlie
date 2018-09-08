4e6a2d85dc18a7e64b88b69703f88d16bc770d9f
Klaudio Sinani <klaudiosinani@protonmail.ch>
2018-09-08 06:29:48 +0300

    Replaced backticks with signle quotes on `empty list` tests

--- test/linear/empty.js
+++ test/linear/empty.js
@@ -25,7 +25,7 @@ test('get node value', t => {
 });
 
 test('next node', t => {
-  t.throws(() => linear.node(0).next, `Cannot read property 'next' of undefined`);
+  t.throws(() => linear.node(0).next, 'Cannot read property \'next\' of undefined');
 });
 
 test('select node', t => {
@@ -37,7 +37,7 @@ test('remove node', t => {
 });
 
 test('set node value', t => {
-  t.throws(() => linear.set({index: 0, value: ''}), `List index out of bounds`);
+  t.throws(() => linear.set({index: 0, value: ''}), 'List index out of bounds');
 });
 
 test('zero length', t => {
