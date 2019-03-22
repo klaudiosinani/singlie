e1bac52c5388ca1124fa2ec5d8d3c306a78cb7e0
Klaudio Sinani <klaudiosinani@protonmail.ch>
2019-03-22 16:40:09 +0200

    Expose `Node` module constructor to the API (#3)

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
 const Circular = require('./src/circular');
 const Linear = require('./src/linear');
+const Node = require('./src/node');
 
-module.exports = Object.assign({}, {Circular}, {Linear});
+module.exports = Object.assign({}, {Circular}, {Linear}, {Node});
