4f780fcafe1c5e1042dbda70a95cf06fae898d3a
Klaudio Sinani <klaudiosinani@protonmail.ch>
2019-03-23 20:02:11 +0200

    TypeScript: Add ambient type declaration tests (#6)

---
+++ test/types/circular.ts
@@ -0,0 +1,57 @@
+'use strict';
+import { Circular } from './../../.';
+
+const circular = new Circular();
+
+// Append a node holding the value `E`
+circular.append('E');
+circular.head; // => E
+circular.last; // => E
+circular.get(0); // => E
+
+// Return the node corresponding to the index
+circular.node(0); // => Node { value: 'E', next: [Circular] }
+circular.node(0).value; // => E
+circular.node(0).next.value; // => E
+circular.node(0).next.next.value; // => E
+
+// Append multiple nodes at once
+circular.append('F', 'G');
+circular.length; // => 3
+circular.node(0).next.value; // => F
+circular.node(0).next.next.value; // => G
+circular.node(0).next.next.next.value; // => E
+circular.toArray(); // => [ 'E', 'F', 'G' ]
+
+// Prepend multiple nodes at once
+circular.prepend('B', 'A');
+circular.join(' '); // => A B E F G
+
+// Insert multiple nodes to the given index
+circular.insert({ value: ['D', 'C', 'X'], index: 2 });
+circular.join(' '); // => A B X C D E F G
+
+// Remove the node corresponding to the index
+circular.remove(2);
+circular.join(' '); // => A B C D E F G
+
+// Update the value of the node corresponding to the index
+circular.node(circular.length - 1).value = '!';
+circular.join(' '); // => A B C D E F !
+circular.set({ value: 'G', index: circular.length - 1 });
+circular.join(' '); // => A B C D E F G
+
+// Iterate over the list
+const array = [];
+circular.forEach(x => array.push(x));
+// => [ 'A', 'B', 'C', 'D', 'E', 'F', 'G' ]
+
+// Chain multiple methods
+circular
+  .reverse()
+  .map(x => `[${x}]`)
+  .join('->');
+// => [G]->[F]->[E]->[D]->[C]->[B]->[A]
+
+// Clear the list
+circular.clear(); // => Circular { head: null, length: 0 }
---
+++ test/types/linear.ts
@@ -0,0 +1,54 @@
+import { Linear } from './../../.';
+
+const linear = new Linear();
+
+// Append a node holding the value `E`
+linear.append('E');
+linear.head; // => E
+linear.last; // => E
+linear.get(0); // => E
+
+// Return the node corresponding to the index
+linear.node(0); // => Node { value: 'E', next: null }
+linear.node(0).value; // => E
+linear.node(0).next; // => null
+
+// Append multiple nodes at once
+linear.append('F', 'G');
+linear.length; // => 3
+linear.node(0).next.value; // => F
+linear.node(0).next.next.value; // => G
+linear.toArray(); // => [ 'E', 'F', 'G' ]
+
+// Prepend multiple nodes at once
+linear.prepend('B', 'A');
+linear.join(' '); // => A B E F G
+
+// Insert multiple nodes to the given index
+linear.insert({ value: ['D', 'C', 'X'], index: 2 });
+linear.join(' '); // => A B X C D E F G
+
+// Remove the node corresponding to the index
+linear.remove(2);
+linear.join(' '); // => A B C D E F G
+
+// Update the value of the node corresponding to the index
+linear.node(linear.length - 1).value = '!';
+linear.join(' '); // => A B C D E F !
+linear.set({ value: 'G', index: linear.length - 1 });
+linear.join(' '); // => A B C D E F G
+
+// Iterate over the list
+const array = [];
+linear.forEach(x => array.push(x));
+// => [ 'A', 'B', 'C', 'D', 'E', 'F', 'G' ]
+
+// Chain multiple methods
+linear
+  .reverse()
+  .map(x => `[${x}]`)
+  .join('->');
+// => [G]->[F]->[E]->[D]->[C]->[B]->[A]
+
+// Clear the list
+linear.clear(); // => Linear { head: null, length: 0 }
