955989bfc22f9c70ea98e751c327cdbd729988ef
Klaus Sinani <klaussinani@gmail.com>
2019-03-24 19:34:53 +0200

    Minor overall improvements on `list` module

--- src/list.js
+++ src/list.js
@@ -5,6 +5,26 @@ class List {
     this._head = null;
     this._length = 0;
   }
+
+  get head() {
+    return this.isEmpty() ? undefined : this._head.value;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  _arrayify(value) {
+    return Array.isArray(value) ? value : [value];
+  }
+
+  _isValid(index) {
+    return index >= 0 && index < this.length;
+  }
+
+  isEmpty() {
+    return !this._head && this._length === 0;
+  }
 }
 
 module.exports = List;
