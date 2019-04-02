5cdd9bf4a08873ab9e2f146cf8ad33074cc90935
Klaudio Sinani <klaudiosinani@protonmail.ch>
2019-04-02 04:53:25 +0300

    Add new `Circular#filter(fn)` class method

--- src/circular.js
+++ src/circular.js
@@ -65,44 +65,16 @@ class Circular extends List {
     return this;
   }
 
-  prepend(...values) {
-    values.forEach(value => {
-      if (this.isEmpty()) {
-        return this._initializeList(value);
-      }
-
-      return this._addHead(value);
-    });
-    return this;
-  }
-
-  reduce(fn, acc) {
-    let result = acc;
+  filter(fn) {
+    const list = new Circular();
 
     this.forEach(x => {
-      result = fn(result, x);
+      if (fn(x)) {
+        list.append(x);
+      }
     });
 
-    return result;
-  }
-
-  insert({value, index = this.length}) {
-    this._arrayify(value).forEach(value => {
-      return (index <= 0) ? this._addHead(value) : this._addNode(value, index);
-    });
-    return this;
-  }
-
-  remove(index = this.length - 1) {
-    if (!this._isValid(index)) {
-      return undefined;
-    }
-
-    if (index === 0) {
-      return (this.length === 1) ? this.clear() : this._removeHead();
-    }
-
-    return this._removeNode(index);
+    return list;
   }
 
   forEach(fn) {
@@ -118,23 +90,11 @@ class Circular extends List {
     return this;
   }
 
-  toArray() {
-    const array = [];
-    this.forEach(x => array.push(x));
-    return array;
-  }
-
-  toLinear() {
-    const Linear = require('./linear');
-    const list = new Linear();
-    this.forEach(x => list.append(x));
-    return list;
-  }
-
-  map(fn) {
-    const list = new Circular();
-    this.forEach(x => list.append(fn(x)));
-    return list;
+  insert({value, index = this.length}) {
+    this._arrayify(value).forEach(value => {
+      return (index <= 0) ? this._addHead(value) : this._addNode(value, index);
+    });
+    return this;
   }
 
   join(separator = ',') {
@@ -156,9 +116,61 @@ class Circular extends List {
     return result;
   }
 
+  map(fn) {
+    const list = new Circular();
+    this.forEach(x => list.append(fn(x)));
+    return list;
+  }
+
+  prepend(...values) {
+    values.forEach(value => {
+      if (this.isEmpty()) {
+        return this._initializeList(value);
+      }
+
+      return this._addHead(value);
+    });
+    return this;
+  }
+
+  reduce(fn, acc) {
+    let result = acc;
+
+    this.forEach(x => {
+      result = fn(result, x);
+    });
+
+    return result;
+  }
+
+  remove(index = this.length - 1) {
+    if (!this._isValid(index)) {
+      return undefined;
+    }
+
+    if (index === 0) {
+      return (this.length === 1) ? this.clear() : this._removeHead();
+    }
+
+    return this._removeNode(index);
+  }
+
   reverse() {
     const list = new Circular();
     this.forEach(x => list.prepend(x));
+    return list;
+  }
+
+  toArray() {
+    const array = [];
+    this.forEach(x => array.push(x));
+    return array;
+  }
+
+  toLinear() {
+    const Linear = require('./linear');
+    const list = new Linear();
+    this.forEach(x => list.append(x));
     return list;
   }
 }
